9c1d2e567dd6ddee4d4b5eeaa2d8263f3a6b8995
Ruslan Karpov <rkarpov88@gmail.com>
2022-12-01 19:56:37 +0200

    finished 2nd task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryItemsMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems  
+
+    .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+        .join('');
+}
+
+
+let modal = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME_PLAYER = "videoplayer-current-time";
+
+setCurrentTime();
+
+const onPlay = (data) => {
+    localStorage.setItem(CURRENT_TIME_PLAYER, data.seconds);
+};
+
+function setCurrentTime() {
+    if (localStorage.getItem(CURRENT_TIME_PLAYER)) {
+        player.setCurrentTime(localStorage.getItem(CURRENT_TIME_PLAYER))
+    }
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
 
